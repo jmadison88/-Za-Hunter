b95d461a9bcf2908d3044ac3891ac197b79d3e33
jmadison <jmad99@sbcglobal.net>
2023-02-13 09:20:02 -0600

    Initial Commit

---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by Josh Madison on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
